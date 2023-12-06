ac5616632118818a0ec86f8a12ab002d84b0b394
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-12-06 13:26:34 -0500

    typescript code ver.1

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Dec 2023
+ * This program sends a string to another MicroBit using radio signals, depending on the distance of the sonar to an object
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(154)
+basic.showIcon(IconNames.Asleep)
+
+// if button a is pressed, sends string 'too close' if distance is less than 10
+input.onButtonPressed(Button.A, function () {
+  // checks distance of sonar to object in front
+  distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  // if the distance is less than 10 cm, project string "too close" to second MicroBit
+  if (distanceToObject <= 10) {
+    radio.sendString('Too Close')
+    basic.showString('Too Close')
+    basic.showIcon(IconNames.Meh)
+  }
+})
+
+// allows to receive strings from other MicroBits on the same frequency.
+radio.onReceivedString(function (receivedString) {
+  basic.clearScreen()
+  basic.showString(receivedString)
+  basic.showIcon(IconNames.Happy)
+})
